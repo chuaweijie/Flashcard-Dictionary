b294f5286f5d58c7412a05eaaf536f820a07b0d1
Chua Wei Jie <chuawei_jie@hotmail.com>
2020-03-28 17:56:31 +0700

    Created Initial Structure for Vocabulary Listing Page
    1. background.js
    - move the insert logic into a function for ease of readability
    - created logic to handle messages from either selection or the pulling of data from the extension itself. 
    - added list_all_records function to retrieve all the records from indexedDB. 
    2. word-list.js
    - The logic behind word-list.html responsible for pulling data and update the page of word-list.html
    3. Added svg logos for the website. 
    4. Options.html
    - Added author and bootstrap
    5. popup.html
    - Added author
    - Added linkage to word-list.html
    6. word-list.html
    - Created the boilerplate of a vocabulary page

--- controllers/background.js
+++ controllers/background.js
@@ -27,6 +27,81 @@ function initialize_indexedDB(){
 	return dbRequest;
 }
 
+function insert_record(request){
+	var dbRequest = initialize_indexedDB();
+	dbRequest.onerror = function(event) {
+		debug && console.log("IndexedDB Error");
+	};
+    dbRequest.onsuccess = function(event) {
+    	var db = event.target.result;
+    	var fcObjectStore = db.transaction("flashcard", "readwrite").objectStore("flashcard");
+    	//Try to check if this vocabulary already existed
+    	var getRequest = fcObjectStore.get(request.vocab);
+
+    	getRequest.onerror = function(event){
+    		debug && console.log("Get Vocab error");
+    		db.close();
+    	}
+
+    	getRequest.onsuccess = function(event){
+    		let data = getRequest.result
+    		let currentDateTime = new Date()
+    		//If no records were found, save the vocab and definition with default values. 
+    		if(data == undefined){
+    			debug && console.log("No records found");
+    			var addRequest = fcObjectStore.add({vocab:request.vocab, definition:request.definition, count:1, creation_date_time:currentDateTime.getTime(), last_test_date_time:0, mastery:0});
+    			addRequest.onerror = function(event){
+		    		debug && console.log("IndexedDB Insertion error: " + request.vocab);
+		    	}
+
+		    	addRequest.onsuccess = function(event) {
+		    		debug && console.log("Save complete: " + request.vocab);
+		    	}
+    		}
+    		//If there is a previosu record, update the values and save it in. 
+    		else{
+    			data.count++;
+    			data.creation_date_time = currentDateTime.getTime();
+    			//Short circuit for if data.mastery > 0, data.mastery--
+    			data.mastery > 0 && data.mastery--;
+    			var addRequest = fcObjectStore.put(data);
+    			addRequest.onerror = function(event){
+		    		debug && console.log("IndexedDB Insertion error: " + request.vocab);
+		    	}
+
+		    	addRequest.onsuccess = function(event) {
+		    		debug && console.log("Save complete: " + request.vocab);
+		    	}
+    		}
+    		db.close();
+    	}
+  	};
+}
+
+function list_all_records(){
+	var dbRequest = initialize_indexedDB();
+	dbRequest.onerror = function(event) {
+		debug && console.log("IndexedDB Error");
+		return false;
+	};
+    dbRequest.onsuccess = function(event) {
+    	var db = event.target.result;
+    	var objectStore = db.transaction("flashcard").objectStore("flashcard");
+    	objectStore.openCursor().onsuccess = function(event) {
+			var cursor = event.target.result;
+			var data = []
+			if (cursor) {
+			    data.push({vocab:cursor.value.vocab, definition:cursor.value.definition, count:cursor.value.count, creation_date_time:cursor.value.creation_date_time, last_test_date_time:cursor.value.last_test_date_time, mastery:cursor.value.mastery});
+			    cursor.continue();
+			}
+			else {
+				console.log("No data");
+			}
+			console.log(data);
+		};
+    }
+}
+
 //Code to launch options page or onboarding page if this is the user's first time. 
 //We can also use this code to display change log after update. 	
 if (localStorage['lastVersionUsed'] != '1') {
@@ -41,52 +116,16 @@ if (localStorage['lastVersionUsed'] != '1') {
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		//Open conneciton to FlashcardDictionary
-		var dbRequest = initialize_indexedDB();
-		dbRequest.onerror = function(event) {
-			debug &&  console.log("IndexedDB Error");
-		};
-	    dbRequest.onsuccess = function(event) {
-	    	var db = event.target.result;
-	    	var fcObjectStore = db.transaction("flashcard", "readwrite").objectStore("flashcard");
-	    	//Try to check if this vocabulary already existed
-	    	var getRequest = fcObjectStore.get(request.vocab);
-
-	    	getRequest.onerror = function(event){
-	    		debug && console.log("Get Vocab error");
-	    	}
-
-	    	getRequest.onsuccess = function(event){
-	    		let data = getRequest.result
-	    		let currentDateTime = new Date()
-	    		//If no records were found, save the vocab and definition with default values. 
-	    		if(data == undefined){
-	    			debug && console.log("No records found");
-	    			var addRequest = fcObjectStore.add({vocab:request.vocab, definition:request.definition, count:1, creation_date_time:currentDateTime.getTime(), last_test_date_time:0, mastery:0});
-	    			addRequest.onerror = function(event){
-			    		debug && console.log("IndexedDB Insertion error: " + request.vocab);
-			    	}
-
-			    	addRequest.onsuccess = function(event) {
-			    		debug && console.log("Save complete: " + request.vocab);
-			    	}
-	    		}
-	    		//If there is a previosu record, update the values and save it in. 
-	    		else{
-	    			data.count++;
-	    			data.creation_date_time = currentDateTime.getTime();
-	    			//Short circuit for if data.mastery > 0, data.mastery--
-	    			data.mastery > 0 && data.mastery--;
-	    			var addRequest = fcObjectStore.put(data);
-	    			addRequest.onerror = function(event){
-			    		debug && console.log("IndexedDB Insertion error: " + request.vocab);
-			    	}
-
-			    	addRequest.onsuccess = function(event) {
-			    		debug && console.log("Save complete: " + request.vocab);
-			    	}
-	    		}
-	    	}
-	  	};	
+		console.log("Sender URL: "+sender.url);
+		//chrome-extension://ekkioomfpehmkanapfbnjhepcehhjphb/views/word-list.html
+		var pattern = /chrome-extension:\/\/.+\/views\/word-list\.html/i;
+		var result = sender.url.match(pattern);
+		if (result == null){
+			insert_record(request);
+		}
+		else{
+			 let data = list_all_records();
+		}
 	}
 );
 
---
+++ controllers/word-list.js
@@ -0,0 +1,2 @@
+chrome.runtime.sendMessage({msg:"Just some random message from word list"});
+
