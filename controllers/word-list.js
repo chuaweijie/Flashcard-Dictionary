c3102841f61a283a091fca66c698419db9951117
Chua Wei Jie <chuawei_jie@hotmail.com>
2020-03-29 08:51:02 +0700

    word-list.js: Message Receiving Logic
    The initial logic to receive message from background.js

--- controllers/word-list.js
+++ controllers/word-list.js
@@ -1,2 +1,10 @@
 chrome.runtime.sendMessage({msg:"Just some random message from word list"});
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    var pattern = /chrome-extension:\/\/.+\/views\/word-list\.html/i;
+	var result = sender.url.match(pattern);
+	
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
